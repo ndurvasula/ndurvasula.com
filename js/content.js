d53448232e9b41c2358dfbd6a8610e2802de8aa5
ndurvasula <140.naveen.d@gmail.com>
2017-04-11 00:19:12 -0400

    First content push
    Resume is not added yet

--- js/content.js
+++ js/content.js
@@ -1,15 +1,33 @@
 var CONTENT = [
     {
-        img: "i1.jpg",
-        id: "p1",
-        title: "Test Post 1",
-        content: "Woot, it instantiates"
+        img: "about.jpg",
+        id: "about",
+        title: "About",
+        content: "Hey there, I'm Naveen. I'm currently a sophomore at Montgomery Blair HS. <br/><br/>\
+                    Stuff I like:\
+                    <br/><ul><li>Research</li><li>Hackathons</li><li>Tennis</li><li>Piano</li></ul>\
+                    Stuff I dislike:\
+                    <br/><ul><li>Game controllers with convex sticks (why?)</li><li>Apple products</li><li>Shoe laces</li></ul>\
+                    Here's my <a href = 'http://github.com/ndurvasula'>GitHub</a> and <a href = 'resume.pdf'>resume</a>."
     },
     {
-        img: "i2.jpg",
+        img: "isef.jpg",
         id: "test",
-        title: "Test Post 2",
-        content: "And the image change"
+        title: "Kidneys",
+        content: "<p>My <a href = 'http://github.com/ndurvasula/KPDMetric'>current ongoing research project</a> is developing a participant-specific estimate of expected organ quality in a kidney paired donation system.\
+                    Essentially, if you are a patient with end-stage renal disease, and wish to enter a kidney paired donation system (a system in which\
+                    patients paired with incompatible donors trade with other such pairs to receive a kidney), my work aims to tell you what the quality of the kidney you will receive is,\
+                    as well as how long it will take for you to receive this kidney. </p>\
+                    <p>To do this, first I Monte Carlo simulated a given patient in a kidney exchange based on published distributions describing demographic data in a steadystate US KPD system, and the codebase \
+                    <a href = 'https://github.com/JohnDickerson/KidneyExchange'>here</a>. By recording the quality of the kidney received, and the waiting time, the expected values of\
+                    these numbers can be computed. Simulating for a sufficient number of trajectories is far too costly, so sampling techniques are necessary to cut down on computation time.</p>\
+                    <p>The sampling method described in section 3 of <a href = 'papers/sampling.pdf'>my paper</a>, with the weighted distance modification in section 6, was implemented and tested.\
+                    The results were pretty bad. Due to the <a href = 'https://en.wikipedia.org/wiki/Curse_of_dimensionality'>curse of dimensionality</a>, there was still an unreasonable amount of\
+                    sampling error when a whopping 10^11 samples were taken. However, trend data indicated that stronger assumptions can be made, and as of now, a Bayesian optimization approach to\
+                    sampling is in the works.</p>\
+                    <p>This work was done under the guidance of <a href = 'http://jpdickerson.com/'>Assistant Prof. John Dickerson</a> and <a href = 'https://www.cs.umd.edu/~srin/'>Prof. Aravind Srinivasan</a>\
+                    from the University of Maryland. I got a mention <a href = 'https://www.cs.umd.edu/article/2017/03/professor-aravind-srinivasan-and-assistant-professor-john-dickerson-mentor-award'>here</a>\
+                    for being an ISEF finalist."
     }
 
 ]
